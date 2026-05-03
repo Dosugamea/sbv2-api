070872d53687acb94021d342748110bcdbe0b998
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2026-05-03 18:42:47 +0900

    fix: 事前生成済みの辞書を無視する カスタム辞書の読み込み時500を吐いていたため

--- crates/sbv2_core/src/jtalk.rs
+++ crates/sbv2_core/src/jtalk.rs
@@ -182,23 +182,10 @@ use std::sync::Arc;
 
 type JPreprocessType = JPreprocess<DefaultTokenizer>;
 
-#[cfg(feature = "agpl_dict")]
-fn agpl_dict() -> Result<Option<UserDictionary>> {
-    Ok(Some(
-        UserDictionary::load(include_bytes!(concat!(env!("OUT_DIR"), "/all.bin")))
-            .map_err(|e| Error::LinderaError(e.to_string()))?,
-    ))
-}
-
-#[cfg(not(feature = "agpl_dict"))]
-fn agpl_dict() -> Result<Option<UserDictionary>> {
-    Ok(None)
-}
-
 fn initialize_jtalk() -> Result<JPreprocessType> {
     let sdic =
         SystemDictionaryConfig::Bundled(kind::JPreprocessDictionaryKind::NaistJdic).load()?;
-    let jpreprocess = JPreprocess::with_dictionaries(sdic, agpl_dict()?);
+    let jpreprocess = JPreprocess::with_dictionaries(sdic, None);
     Ok(jpreprocess)
 }
 
